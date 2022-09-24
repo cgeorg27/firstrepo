cfd6d09c2ae52fb128b7f43fdb03f25843909712
cgeorg27 <cgeorg27@illinois.edu>
2022-09-24 15:57:23 -0500

    Push1

--- Scraper/Scaper.py
+++ Scraper/Scaper.py
@@ -1,4 +1,5 @@
 import re
+from turtle import title
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
@@ -11,3 +12,18 @@ request_page.close()
 html_soup = BeautifulSoup(page_html, 'html.parser')
 
 cactus_items = html_soup.find_all('div', class_="grid-product__content")
+
+filename = 'products1.csv'
+f = open(filename, 'w')
+
+headers = 'Title, Price \n'
+
+f.write(headers)
+
+for cactus in cactus_items:
+    title = cactus.find('span', class_="snize-title").text
+    price = cactus.find('span', class_="snize-price  money ").text
+    
+    f.write(title + ',' + price)
+ 
+f.close() 
